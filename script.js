7c71d159f7c15ad5b5c624ffc44b77b588caf34f
Esteban Garibaldi <estebangaribaldibmd@gmail.com>
2024-08-26 15:36:42 -0700

    Implementacion logica js, html y css

---
+++ script.js
@@ -0,0 +1,62 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+const leyenda = document.querySelector(".leyenda");
+const botonCopiar = document.querySelector(".copiar");
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+    leyenda.textContent = "Tu texto encriptado es:";
+    actualizarBotonCopiar(); // Actualiza la visibilidad del botón Copiar
+}
+
+function encriptar(stringEncriptado) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptado = stringEncriptado.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptado.includes(matrizCodigo[i][0])) {
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptado;
+}
+
+function btnDesencriptar() {
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensaje.value = textoDesencriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+    leyenda.textContent = "Tu texto desencriptado es:";
+    actualizarBotonCopiar(); // Actualiza la visibilidad del botón Copiar
+}
+
+function desencriptar(stringDesencriptado) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptado = stringDesencriptado.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptado.includes(matrizCodigo[i][1])) {
+            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+    return stringDesencriptado;
+}
+
+function copiarTexto() {
+    mensaje.select();
+    document.execCommand('copy');
+}
+
+function actualizarBotonCopiar() {
+    if (mensaje.value.trim() === "") {
+        botonCopiar.style.display = "none"; // Oculta el botón si el recuadro está vacío
+    } else {
+        botonCopiar.style.display = "block"; // Muestra el botón si hay texto
+    }
+}
+
+// Inicializar la visibilidad del botón cuando la página carga
+document.addEventListener("DOMContentLoaded", actualizarBotonCopiar);
